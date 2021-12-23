b7e027789460c03039af6f45754caffc63947146
tangc <1654169282@qq.com>
2021-12-23 16:10:20 +0800

    first commit

---
+++ src/test/java/top/cardio/learntool/LearntoolApplicationTests.java
@@ -0,0 +1,13 @@
+package top.cardio.learntool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearntoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
