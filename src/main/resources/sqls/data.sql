6c1ffa967d1369c52bf69612abe813b727007e6e
tangc <1654169282@qq.com>
2021-12-24 17:54:36 +0800

    init sql

---
+++ src/main/resources/sqls/data.sql
@@ -0,0 +1,5 @@
+insert into `category`(category, date_created, last_updated)
+values('code', NOW(), NOW());
+
+insert into `category`(category, date_created, last_updated)
+values('linux', NOW(), NOW());
---
+++ src/main/resources/sqls/ddl.sql
@@ -0,0 +1,30 @@
+create table `blog`
+(
+    id           bigint(20)  not null auto_increment primary key,
+    title        varchar(64) not null,
+    file_path    varchar(128),
+    date_created timestamp,
+    last_updated timestamp,
+    create_by    bigint(20),
+    update_by    bigint(20)
+);
+
+create table `keyword`
+(
+    id           bigint(20)  not null auto_increment primary key,
+    keyword      varchar(64) not null,
+    date_created timestamp,
+    last_updated timestamp,
+    create_by    bigint(20),
+    update_by    bigint(20)
+);
+
+create table `category`
+(
+    id           bigint(20)  not null auto_increment primary key,
+    category     varchar(32) not null,
+    date_created timestamp,
+    last_updated timestamp,
+    create_by    bigint(20),
+    update_by    bigint(20)
+)
